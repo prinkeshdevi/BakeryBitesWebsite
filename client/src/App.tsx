812aa467b4efa49fe47a2758427f9096777b7773
Prinkesh Devi <prinkeshdevi@gmail.com>
2025-10-27 20:52:11 +0000

    feat: add OwnAdmin page with authentication and management tabs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import Admin from "@/pages/Admin";
+import OwnAdmin from "@/pages/OwnAdmin";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -12,6 +13,7 @@ function Router() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/admin" component={Admin} />
+      <Route path="/ownadmin" component={OwnAdmin} />
       <Route component={NotFound} />
     </Switch>
   );
